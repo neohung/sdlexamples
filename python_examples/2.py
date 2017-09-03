6b01058203fde826c8e0abdfcb515f43f06919e1
neo <neo@Mindyde-MacBook-Air.local>
2017-09-03 23:27:12 +0800

    test

--- python_examples/2.py
+++ python_examples/2.py
@@ -4,6 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from scipy.io import wavfile
 
 class SimpleHMM:
 	def __init__(self):
@@ -15,11 +16,19 @@ class SimpleHMM:
 		self.N = len(self.states)
 		self.M = len(self.observations)
 		#Define Model
+		"""
 		transition_probability = [[0.7, 0.3],
 		                          [0.4, 0.6]]
 		emission_probability = [[0.5, 0.4, 0.1],
 		                        [0.1, 0.3, 0.6]]
 		start_probability = [0.6, 0.4]
+		"""
+		transition_probability= [[0.8125,0.1875],[0.2,0.8]]
+		emission_probability = [[0.875,0.125],[0.25,0.75]]
+		start_probability = [0.5,0.5]
+		self.N = len(transition_probability)
+		self.M = len(emission_probability[0])
+		
 		self.A = transition_probability
 		self.B = emission_probability
 		self.pi = start_probability
@@ -28,44 +37,53 @@ class SimpleHMM:
 		self.T = len(obs_seq)
 		#print 'forward o-> {0}' . format(obs_seq)
 		#x[T][N]
-		self.x = np.array( np.zeros((self.T,self.N)), dtype=np.float)
+		self.alpha = np.array( np.zeros((self.T,self.N)), dtype=np.float)
 		#t=1:
 		for i in range (0,self.N,1):
-			self.x[0][i] = self.pi[i] * self.B[i][obs_seq[0]]
+			self.alpha[0][i] = self.pi[i] * self.B[i][obs_seq[0]]
 
+		result = np.array( np.zeros((self.T)), dtype=np.float)
 		for t in range (1,self.T,1):
+			#because t = 1 ~ T-1, so result start from t-1
+			result[t-1] = 0
 			for i in range (0,self.N,1):
 				temp = 0
 				for j in range (0,self.N,1):
-					temp += self.x[t-1][j]*self.A[j][i] 
-				self.x[t][i] = temp * self.B[i][obs_seq[t]]				
-		#print 'x: {0}' . format(self.x)
-		result = 0
+					temp += self.alpha[t-1][j]*self.A[j][i] 
+				self.alpha[t][i] = temp * self.B[i][obs_seq[t]]
+				result[t-1] += self.alpha[t][i]
+
+		logresult = 0
+		for t in range (0,self.T-1,1):
+			logresult += np.log(result[t])
+		return logresult
+		#print 'x: {0}' . format(self.alpha)
+		#result = 0
 		#Use t-1 to reference 
-		for i in range (0,self.N,1):
-			result += self.x[self.T-1][i]
+		#for i in range (0,self.N,1):
+		#	result += self.alpha[self.T-1][i]
 		#print "forward result: %f" % result
-		return result
+		#return np.log(result)
 
 	def backward(self, obs_seq):
 		self.T = len(obs_seq)
 		#print 'backward: o-> {0}' . format(obs_seq)
-		self.y = np.array( np.zeros((self.T,self.N)), dtype=np.float)
+		self.beta = np.array( np.zeros((self.T,self.N)), dtype=np.float)
 		#t=T-1:		
 		for i in range (0,self.N,1):
-			self.y[self.T-1][i] = 1
+			self.beta[self.T-1][i] = 1
 		t = self.T-2
 		while (t >=0):
 			for i in range (0,self.N,1):
 				for j in range (0,self.N,1):
-					self.y[t][i] += self.y[t+1][j] * self.A[i][j] * self.B[j][obs_seq[t+1]]	
+					self.beta[t][i] += self.beta[t+1][j] * self.A[i][j] * self.B[j][obs_seq[t+1]]	
 			t-=1
 		#Consider O1 and Pi
 		#print 'y: {0}' . format(self.y)
 		result = 0
 		#Use t-1 to reference 
 		for i in range (0,self.N,1):
-			result += self.y[0][i] * self.pi[i] * self.B[i][obs_seq[0]]	
+			result += self.beta[0][i] * self.pi[i] * self.B[i][obs_seq[0]]	
 		#print "backward result: %f" % result
 		return result
 	#給定obs_seq在時刻t看到obs_seq(t)的前提下, 預估obs_seq(t)屬於狀態s的機率
@@ -73,14 +91,14 @@ class SimpleHMM:
 		self.forward(obs_seq)
 		self.backward(obs_seq)
 		time = t
-		probability_from_forward = self.x[time][s]
-		probability_from_backward = self.y[time][s]
+		probability_from_forward = self.alpha[time][s]
+		probability_from_backward = self.beta[time][s]
 		val = probability_from_forward * probability_from_backward
 		#print probability_from_forward
 		#print probability_from_backward
 		total = 0
 		for i in range (0,self.N,1):
-			total += self.x[t][i] * self.y[time][i]
+			total += self.alpha[t][i] * self.beta[time][i]
 		return val/total
 
 	def viterbi(self, obs_seq):
@@ -139,11 +157,131 @@ class SimpleHMM:
 		#print 'state_path: {0}' . format(self.viterbi_state_path)
 		#print "max_probability=%f" % max_probability
 		return self.viterbi_state_path
+
+	def computeGamma(self):
+		#print "computeGamma"
+		self.gamma = np.array( np.zeros((self.T,self.N)), dtype=np.float)
+		for t in range (0,self.T,1):
+			denominator = 0.0
+			for i in range (0,self.N,1):
+				self.gamma[t][i] = self.alpha[t][i] * self.beta[t][i]
+				denominator +=  self.gamma[t][i]
+			for i in range (0,self.N,1):
+				self.gamma[t][i] /= denominator
+	
+	def computeXi(self, obs_seq):
+		#print "computeXi"
+		self.xi = np.array( np.zeros((self.T,self.N,self.N)), dtype=np.float)
+		for t in range (0,self.T-1,1):
+			denominator = 0.0
+			for i in range (0,self.N,1):
+				for j in range (0,self.N,1):
+					self.xi[t][i][j] = self.alpha[t][i] * self.beta[t+1][j] * self.A[i][j] * self.B[j][obs_seq[t*1]]
+					denominator +=  self.xi[t][i][j]
+			for i in range (0,self.N,1):
+				for j in range (0,self.N,1):
+					self.xi[t][i][j] /= denominator
+
+
+	def BaumWelch(self, O):
+		print "BaumWelch"
+		L = len(O)
+		newpi = np.array( np.zeros((self.N)), dtype=np.float)
+		newA = np.array( np.zeros((self.N,self.N)), dtype=np.float)
+		newB = np.array( np.zeros((self.N,self.M)), dtype=np.float)
+		denominator_newA = np.array( np.zeros((self.N)), dtype=np.float)
+		denominator_newB = np.array( np.zeros((self.N)), dtype=np.float)
+		Count = 0
+		prev_logp = 0
+		prev_delta = 10e-70
+		ratio = 1
+		while True:
+			Count+=1
+			# E - step, cal alpha, beta by obs_seq, A, B and pi, 
+			for l in range (0,L,1):
+				logp = self.forward(O[l])
+				self.backward(O[l])
+				self.computeGamma()
+				self.computeXi(O[l])		
+				for i in range (0,self.N,1):
+					newpi[i] += self.gamma[0][i]
+					#0~T-2 --> A, 0~T-1 --> B
+					for t in range (0,self.T-1,1):
+						denominator_newA[i] += self.gamma[t][i]
+						denominator_newB[i] += self.gamma[t][i]
+					denominator_newB[i] += self.gamma[self.T-1][i]
+					#0~T-1
+					for j in range (0,self.N,1):
+						for t in range (0,self.T-1,1):
+							newA[i][j] += self.xi[t][i][j]
+					#
+					for k in range (0,self.M,1):
+						for t in range (0,self.T-1,1):
+							if (O[l][t] == k):
+								newB[i][k] += self.gamma[t][i]
+
+			# M - step, recal A, B, pi
+			for i in range (0,self.N,1):
+				self.pi[i] = 0.001*(1/self.N) + 0.999*(newpi[i]/L)
+				for j in range (0,self.N,1):
+					self.A[i][j] =  0.001*(1/self.N) + 0.999*(newA[i][j]/denominator_newA[i])
+					newA[i][j] = 0
+
+				for k in range (0,self.M,1):
+					self.B[i][k] =  0.001*(1/self.M) + 0.999*(newB[i][k]/denominator_newB[i])
+					newB[i][k] = 0
+
+				denominator_newA[i] = 0
+				denominator_newB[i] = 0
+				newpi[i] = 0
+
+
+			#print logp
+			delta = logp - prev_logp
+			prev_logp = logp
+			if (Count > 1):
+				ratio = delta / prev_delta
+				print "r=%f" % ratio
+				#print logp
+			prev_delta = delta
+
+
+			#newA = np.array( np.zeros((self.N,self.N)), dtype=np.float)
+			#newB = np.array( np.zeros((self.N,self.M)), dtype=np.float)
+			#denominator_newA = np.array( np.zeros((self.N,self.N)), dtype=np.float)
+			#denominator_newB = np.array( np.zeros((self.N,self.M)), dtype=np.float)
+			if (Count > 3000):
+				print self.pi
+				print self.A
+				print self.B
+				
+				break
+			if ((ratio >= 0) and (ratio < 0.01)):
+				print "Count=%d" % Count
+				print self.pi
+				print self.A
+				print self.B
+				break
+		"""
+		while True:
+			# E - step
+			for l in range (0,L,1):
+				self.forward(O[0])
+				self.backward(O[0])
+				self.computeGamma()
+				#self.computeXi(O[l])
+				for i in range (0,self.N,1):
+					self.pi[i] = 0
+				print O[l]
+			
+			break
+		"""
 		
 def main():
 	print "%s" % (u"Start")
 	test = SimpleHMM()
-	o = [test.observations['dizzy'],
+	"""
+	o1 = [test.observations['dizzy'],
 		 test.observations['normal'],
 		 test.observations['normal'],
 		 test.observations['normal'],
@@ -155,9 +293,33 @@ def main():
 		 test.observations['cold'],
 		 test.observations['cold'],
 		 test.observations['dizzy']]
+
+	o2 = [test.observations['normal'],
+		 test.observations['normal'],
+		 test.observations['normal'],
+		 test.observations['normal'],
+		 test.observations['normal'],
+		 test.observations['dizzy'],
+		 test.observations['cold'],
+		 test.observations['cold'],
+		 test.observations['cold'],
+		 test.observations['cold'],
+		 test.observations['cold'],
+		 test.observations['normal']]
+
+	O = [ o1 , o2]
+	"""
+	O = [[1,0,0,1,1,0,0,0,0],
+        [1,1,0,1,0,0,1,1,0],
+        [0,0,1,1,0,0,1,1,1]]
 	#print "%s: %f" % (test.ylabel[0], test.get_state_probability_in_time(o,len(o)-1,test.states[test.ylabel[0]]))
 	#print "%s: %f" % (test.ylabel[1], test.get_state_probability_in_time(o,len(o)-1,test.states[test.ylabel[1]]))
-	print "viterbi: {0}" . format(test.viterbi(o)) 
+	#print "viterbi: {0}" . format(test.viterbi(o)) 
+	#test.computeGamma(o)
+	#test.computeXi(o)
+	#test.BaumWelch(O)
+	_, d = wavfile.read("audio/apple/apple01.wav")
+	print d.shape[0]
 	print "%s" % (u"End")
 
 if __name__ == '__main__':
--- python_examples/4.py
+++ python_examples/4.py
@@ -1,49 +1,100 @@
 #!/usr/bin/python
 import numpy as np
-import random
-import collections
+#import random
+#import collections
 import matplotlib.pyplot as plt
-import math
-import scipy.stats
+#import math
+#import scipy.stats
+from scipy.io import wavfile
+import scipy
+from numpy.lib.stride_tricks import as_strided
 
-def p_func_raw(x, a, b):
-    S1 = ((b ** a) / math.gamma(a))
-    S2 = x ** (a - 1)
-    S3 = math.exp(-b * x)
-    return S1 * S2 * S3  # * S4
+#Short Time Fourier Transform (STFT)
+def stft(x, fftsize=64, overlap_pct=.5):   
+    #Modified from http://stackoverflow.com/questions/2459295/stft-and-istft-in-python
+    hop = int(fftsize * (1 - overlap_pct))
+    w = scipy.hanning(fftsize + 1)[:-1]    
+    raw = np.array([np.fft.rfft(w * x[i:i + fftsize]) for i in range(0, len(x) - fftsize, hop)])
+    return raw[:, :(fftsize // 2)]
 
-
-def p_func(y):
-    return p_func_raw(y, 2, 1)
-
-def q_func(beta):
-    return np.random.exponential(beta)
-
-def q_func_pdf(x, beta):
-    return scipy.stats.expon.pdf(x, scale=beta)
-
-def metrohast(M):
-    X = []
-    beta = 5.
-    xt = beta
-    for i in range(M):
-        aj = q_func(beta)
-        c = q_func_pdf(xt, beta) / q_func_pdf(aj, beta)
-        alpha = min(1., (p_func(aj) / p_func(xt)) * c)
-        if random.random() <= alpha:
-            xt = aj
-        X.append(xt)
-    return X
-
-def draw(S):
-    n, bins, patches = plt.hist(S, 100, normed=1, facecolor='b', alpha=0.2)
-    plt.plot(bins, [p_func(x) for x in bins], color='r')
-    plt.show()
+#Peak detection using the technique described here: http://kkjkok.blogspot.com/2013/12/dsp-snippets_9.html 
+def peakfind(x, n_peaks, l_size=3, r_size=3, c_size=3, f=np.mean):
+    win_size = l_size + r_size + c_size
+    shape = x.shape[:-1] + (x.shape[-1] - win_size + 1, win_size)
+    strides = x.strides + (x.strides[-1],)
+    xs = as_strided(x, shape=shape, strides=strides)
+    def is_peak(x):
+        centered = (np.argmax(x) == l_size + int(c_size/2))
+        l = x[:l_size]
+        c = x[l_size:l_size + c_size]
+        r = x[-r_size:]
+        passes = np.max(c) > np.max([f(l), f(r)])
+        if centered and passes:
+            return np.max(c)
+        else:
+            return -1
+    r = np.apply_along_axis(is_peak, 1, xs)
+    top = np.argsort(r, None)[::-1]
+    heights = r[top[:n_peaks]]
+    #Add l_size and half - 1 of center size to get to actual peak location
+    top[top > -1] = top[top > -1] + l_size + int(c_size / 2.)
+    return heights, top[:n_peaks]
 
 def main():
-    X = metrohast(1000)
-    #print X   
-    draw(X)
+    filenames = [ "audio/apple/apple01.wav",
+                  "audio/apple/apple02.wav",
+                  "audio/apple/apple03.wav",
+                  "audio/apple/apple04.wav",
+                  "audio/apple/apple05.wav",
+                  "audio/apple/apple06.wav",
+                  "audio/apple/apple07.wav",
+                  "audio/apple/apple08.wav",
+                  "audio/apple/apple09.wav",
+                  "audio/apple/apple10.wav",
+                  ]
+    data = np.zeros((len(filenames), 32000))
+    for n,file in enumerate(filenames):
+        _, d = wavfile.read(file)
+        print "(%d)Size: %d" % (n, d.shape[0])
+        data[n, :d.shape[0]] = d
+    print('Total have %d data:' % data.shape[0])
+    plt.plot(data[0, :], color='steelblue')
+    plt.title('Timeseries example for %s'% "Apple01")
+    plt.xlim(0, 3500)
+    plt.xlabel('Time (samples)')
+    plt.ylabel('Amplitude (signed 16 bit)')
+    plt.figure()
+    log_freq = 20 * np.log(np.abs(stft(data[0, :])) + 1)
+    #print(log_freq.shape)
+    plt.imshow(log_freq, cmap='gray', interpolation=None)
+    plt.xlabel('Freq (bin)')
+    plt.ylabel('Time (overlapped frames)')
+    plt.ylim(log_freq.shape[1])
+    plt.title('PSD of %s example'% "Apple01")
+    plt.figure()
+    #Plot dim [1x32] in index=15
+    index = 2
+    plot_data = np.abs(stft(data[0, :]))[index, :]
+    plt.plot(plot_data, color='steelblue')
+    #plt.plot(fp, fv, 'x', color='darkred')
+    plt.title('Peak location example')
+    plt.xlabel('Frequency (bins)')
+    plt.ylabel('Amplitude')
+
+    values, locs = peakfind(plot_data, n_peaks=6)
+    fp = locs[values > -1]
+    fv = values[values > -1]
+    print fv
+    print fp
+    plt.plot(fp, fv, 'x', color='darkred')
+    #print plot_data.shape
+    obs = np.zeros((6, data.shape[0]))
+    for r in range(data.shape[0]):
+        _, t = peakfind(data[r, :], n_peaks=6)
+        obs[:, r] = t.copy()
+    print obs
+    #plt.plot(obs[0,index], fv, 'x', color='darkred')
+    plt.show()
 
 if __name__ == '__main__':
     main()
